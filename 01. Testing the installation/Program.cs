30cca315d44314879b2fdbfda86f7e2b992a290f
japgo <japgo@naver.com>
2018-11-12 16:28:16 +0900

    add

---
+++ 01. Testing the installation/Program.cs
@@ -0,0 +1,41 @@
+﻿using Matrox.MatroxImagingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Testing_the_installation {
+	class Program {
+		static void Main( string[] args ) {
+			MIL_ID MilApplication = new MIL_ID();
+			MIL_ID MilSystem = new MIL_ID();
+			MIL_ID MilDisplay = new MIL_ID();
+			MIL_ID MilImage = new MIL_ID();
+
+			MIL.MappAllocDefault( MIL.M_DEFAULT, ref MilApplication, ref MilSystem, ref MilDisplay, MIL.M_NULL, ref MilImage );
+
+			MIL_INT ret = MIL.MappGetError( MIL.M_GLOBAL, MIL.M_NULL );
+			if( ret != 0 ) {
+				Console.WriteLine( "System allocation error." );
+				MIL.MappFreeDefault( MilApplication, MilSystem, MilDisplay, MIL.M_NULL, MilImage );
+				return;
+			}
+
+
+			MIL.MgraColor( MIL.M_DEFAULT, 0xF0 );
+			MIL.MgraFont( MIL.M_DEFAULT, MIL.M_FONT_DEFAULT_LARGE );
+			MIL.MgraText( MIL.M_DEFAULT, MilImage, 160L, 230L, " Welcome to MIL !!! " );
+			MIL.MgraColor( MIL.M_DEFAULT, 0xC0 );
+			MIL.MgraRect( MIL.M_DEFAULT, MilImage, 100L, 150L, 530L, 340L );
+			MIL.MgraRect( MIL.M_DEFAULT, MilImage, 120L, 170L, 510L, 320L );
+			MIL.MgraRect( MIL.M_DEFAULT, MilImage, 140L, 190L, 490L, 300L );
+
+			Console.WriteLine( "System allocation successful." );
+
+			Console.ReadKey();
+
+			MIL.MappFreeDefault( MilApplication, MilSystem, MilDisplay, MIL.M_NULL, MilImage );
+		}
+	}
+}
---
+++ 02. Allocating and displaying an image buffer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Matrox.MatroxImagingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02.Allocating_and_displaying_an_image_buffer {
+	class Program : MIL{
+		static void Main( string[] args ) {
+			MIL_ID MilApplication = new MIL_ID();
+			MIL_ID MilSystem = new MIL_ID();
+			MIL_ID MilDisplay = new MIL_ID();
+			MIL_ID MilImage = new MIL_ID();
+
+			MappAlloc( M_DEFAULT, ref MilApplication );
+			MsysAlloc( M_SYSTEM_GPU, M_DEFAULT, M_DEFAULT, ref MilSystem );
+			MdispAlloc( MilSystem, M_DEFAULT, "M_DEFAULT", M_WINDOWED, ref MilDisplay );
+
+			MbufAlloc2d( MilSystem, 512, 512, 8 + M_UNSIGNED, M_IMAGE + M_DISP + M_GRAB, ref MilImage );
+
+			MdispSelect( MilDisplay, MilImage );
+			for( int i = 0; i < 10; i++ ) {
+				MbufClear( MilImage, 0L );
+				Thread.Sleep( 250 );
+				MgraArcFill( M_DEFAULT, MilImage, 256L, 240L, 100L, 100L, 0.0, 360.0 );
+				Thread.Sleep( 250 );
+				MgraText( M_DEFAULT, MilImage, 238L, 234L, " MIL " );
+				Thread.Sleep( 250 );
+			}
+
+			Console.WriteLine( "A circle was drawn in the displayed image buffer." );
+			Console.ReadKey();
+
+			MbufFree( MilImage );
+			MdispFree( MilDisplay );
+			MsysFree( MilSystem );
+			MappFree( MilApplication );
+		}
+	}
+}
